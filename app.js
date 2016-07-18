7b298c35ca570a1ef745bd27a09fb2b05790a525
Andrei Railean <andrei@siter.com.au>
2016-07-19 07:02:55 +1000

    init repo and add first examples

---
+++ app.js
@@ -0,0 +1,26 @@
+var foo
+
+var myArray = [
+  'a',
+  'b',
+   'c'
+  ]
+
+var myObject = {
+  'name' : 'Object' ,
+  purpose: 'Testing',
+  "version" :1
+}
+
+
+function addTwo(one,two){
+  var sum = one+two
+  return
+}
+
+
+function getGreeting(name) {
+  return ('Hello, '+name + '! Write good code, please!');
+}
+
+console.log(getGreeting('SydJS'));
