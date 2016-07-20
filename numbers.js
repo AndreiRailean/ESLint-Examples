5d39e88917749524e17f189251540f975dc1bc19
Andrei Railean <andrei@siter.com.au>
2016-07-20 21:01:49 +1000

    new prime found

--- numbers.js
+++ numbers.js
@@ -1,7 +1,7 @@
 'use strict'
 
-export const primes = [2, 3, 5]
+export const primes = [2, 3, 5, 7]
 
-export const doubles = primes.map(function (x) {
+export const doubles = primes.map((x) => {
   return x * 2
 })
