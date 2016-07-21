67249b542ca50c0b6ef4b70a3ce301d8bfc44a5c
Andrei Railean <andrei@siter.com.au>
2016-07-21 11:42:14 +1000

    Revert "new prime found"
    This reverts commit 5d39e88917749524e17f189251540f975dc1bc19.

--- numbers.js
+++ numbers.js
@@ -1,7 +1,7 @@
 'use strict'
 
-export const primes = [2, 3, 5, 7]
+export const primes = [2, 3, 5]
 
-export const doubles = primes.map((x) => {
+export const doubles = primes.map(function (x) {
   return x * 2
 })
